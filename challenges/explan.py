f4662c0d8d73e577f805c9867c8681a73e06838f
Khavushka <khavushka@gmail.com>
2023-04-06 17:54:38 +0200

    random colour

--- challenges/explan.py
+++ challenges/explan.py
@@ -7,3 +7,18 @@ display a random integer between 1 and 100 inclusive.
 '''
 
 import random
+
+num = random.randint(0, 100)
+print(num)
+
+
+'''
+display five colours and ask the user 
+to pick one. if they pick the same as the program has choisen, say
+"Well done", otherwise display a witty answer which involves the correct colour, e.g. "I bet you are Green with envy"
+or " You are probably feeling Blue right now".
+Ask them to guess again; if they have still not got it right, keep giving them the same clue and ask user to enter a colour until they guess it correctly.
+'''
+
+colour = random.choice(['green', 'yellow'])
+print(colour)
