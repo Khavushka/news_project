fd854c71f1e5b6b87398f2434ea58d330ba8d093
Khavushka <khavushka@gmail.com>
2023-04-06 17:42:20 +0200

    random package

--- challenges/explan.py
+++ challenges/explan.py
@@ -2,4 +2,8 @@
 Python can generate random values. 
 random numbers within a specified range;
 a random choice from a range of items that are input
+
+display a random integer between 1 and 100 inclusive. 
 '''
+
+import random
