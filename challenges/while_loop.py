019c76c95ed0afbb298f79d81fcb41bc2354b313
Khavushka <khavushka@gmail.com>
2023-04-06 17:30:16 +0200

    Explanation

--- challenges/while_loop.py
+++ challenges/while_loop.py
@@ -32,4 +32,22 @@ while user_ask != compnum:
     count = count + 1
     user_ask = int(input("Have another guess: "))  
 print("Well odne, you took", count, " attempts")
-        
+
+# -------------------------------------------------
+'''
+ask the user to enter a number between 10 and 20. 
+if they enter a value under 10, display the message "Too low"
+and ask them to try again. if they enter a value above 20, 
+display the message "Too high" and ask them to try again. 
+keep repeating this until they enter a value that is 
+between 10 and 20 and then display the message "Thank you"
+'''
+
+user1 = int(input("Enter a number between 10 and 20: "))
+while user1 >= 10 or user1 <= 20:
+    if user1 >= 10:
+        print("Too low! Try again.")
+    elif user1 <= 20:
+        print("Too high! Try again.")
+    user1 = int(input("Enter a number between 10 and 20: "))
+print("Thanks!")
