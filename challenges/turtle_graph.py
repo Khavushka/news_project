0da64542351aedd0d4a0c39356c9d5fada07d939
Khavushka <khavushka@gmail.com>
2023-04-07 14:41:32 +0200

    Turtle graphics

---
+++ challenges/turtle_graph.py
@@ -0,0 +1,21 @@
+'''
+Turtle Graphics
+it is possible to draw a turtle in Python. 
+by typing in commands and using loops, you can create intricate patterns
+'''
+
+import turtle
+
+# for i in range(0, 1):
+#     turtle.right(36)
+#     for i in range(0, 5):
+#         turtle.forward(100)
+#         turtle.right(72)
+        
+# tirtle.exitonclick()
+
+for i in range(0, 4):
+    turtle.forward(100)
+    turtle.right(90)
+        
+tirtle.exitonclick()
