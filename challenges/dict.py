7aac19ef13c84c5986902dac7489f3190b3499b4
Khavushka <khavushka@gmail.com>
2023-04-08 23:13:34 +0200

    new assigment

---
+++ challenges/dict.py
@@ -0,0 +1,6 @@
+'''
+after gathering the four names, ages and shoe sizes,
+ask the user to enter the name of the person they want to remove
+from the list. delete this row from the data and display the other
+rows on separate lines
+'''
