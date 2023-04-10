433b491f24a11271a35408e5799e2ea10fb2f09c
Khavushka <khavushka@gmail.com>
2023-04-10 22:41:39 +0200

    two different solutions

--- challenges/dict.py
+++ challenges/dict.py
@@ -8,20 +8,25 @@ create the following using a simple 2d list using the standard python indexing.
 '''
 after gathering the four names, ages and shoe sizes, ask the user to enter the name of the person they want to remove from the list. delete this row from the data and display the other rows on separate lines
 '''
-four_names = [{'Susan':{'Ma': 45, "En": 37},"Peter":{"Ma":62,"En": 46}}]
+# four_names = {'Susan':{'Ma': 45, "En": 37},"Peter":{"Ma":62,"En": 46}}
+# # print(four_names)
+# user = input("Enter name you want to remove from the list: ")
+# if user in four_names:
+#     del four_names[user]
+#     print(user)
 # print(four_names)
-user = input("Enter name you want to remove from the list: ")
-if user in four_names:
-    del four_names[user]
-    print(user)
-print(four_names)
 
 # second solution
-list = {}
-for i in range(0,4):
-    name = input("Enter your name: ")
-    age = input("Enter your age: ")
-    shoe_size = input("Enter your shoe size: ")
-    list[name] = {"Age": age, "Shoe Size": shoe_size}
+# list = {}
+# for i in range(0,2):
+#     name = input("Enter your name: ")
+#     age = int(input("Enter your age: "))
+#     shoe_size = int(input("Enter your shoe size: "))
+#     list[name] = {"Age": age, "Shoe Size": shoe_size}
+# print(list)
     
-user_int = input("Enter name you want to remove from the list: ")
+# user_int = input("Enter name you want to remove from the list: ")
+# del list[user_int]
+
+# for name in list:
+#     print(name)
