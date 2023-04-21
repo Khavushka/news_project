b4c3be65e3992d0fa882c678ad4aca2655c94077
Khavushka <khavushka@gmail.com>
2023-04-21 07:56:02 +0200

    Reading and writing to a text file

--- challenges/text_file.py
+++ challenges/text_file.py
@@ -27,9 +27,32 @@ if they select 3, ask the user to enter a new subject and save it to the file an
 run the program several times to test the options 
 '''
 
-file = open("selection.txt", "w")
-file.write("1,\n 2,\n 3")
-file = open("selection.txt", "x")
-user_select=input("Select 1, 2 or 3: ")
-file.write(user_select, "\n")
-file.close()
+# file = open("selection.txt", "w")
+# file.write("1\n2\n3")
+# # file = open("selection.txt", "x")
+# user_select=input("Select 1, 2 or 3: ")
+# file.write(user_select + "\n")
+# file.close()
+
+print("1: Create a new file")
+print("2: Display the file")
+print("3: Add a new item to the file")
+selection = int(input("Make a selection 1, 2 or 3: "))
+if selection == 1:
+    subject = input("Enter a school subject: ")
+    file = open("Subject.txt", "w")
+    file.write(subject + "\n")
+    file.close()
+elif selection == 2:
+    file = open("Subject.txt", "r")
+    print(file.read())
+elif selection == 3:
+    file = open("Subject.txt", "a")
+    subject = input("Enter a school subject: ")
+    file.write(subject + "\n")
+    file.close()
+    file = open("Subject.txt", "r")
+    print(file.read())
+else:
+    print("Invalid option")
+    
