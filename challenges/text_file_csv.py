0bc562f3a97792cb819924970c122c1788a5a13e
Khavushka <khavushka@gmail.com>
2023-04-26 14:57:41 +0200

    different methods in python .csv

--- challenges/text_file_csv.py
+++ challenges/text_file_csv.py
@@ -9,11 +9,29 @@ Using the Books.csv file, ask the user how many records they want to add to the 
 After all the data has been added, ask for an author and display all the books in the list by that author. 
 If there are no books by that author in the list, display a suitable message.
 '''
-file = open("Books.csv", "r")
-records = input("Please enter records, you want to add to the list: ")
-reader = csv.reader(file)
-for row in file:
-    if records in row:
-        print(row)
+records = int(input("How many books you want to add to the list?  "))
+file = open("Books.csv", "a")
 
+for info in range(0, records):
+    title = input("Title: ")
+    author = input("Author: ")
+    year = input("Year: ")
+    newrecord = title + ", " + author + ", " + year + "\n"
+    file.write(str(newrecord))
 file.close()
+
+fileSearch = input("Search after book: ")
+file = open("Books.csv", "r")
+count = 0
+for book in file:
+    if fileSearch in str(book):
+        print(book)
+if count == 0:
+        print("There are no books by that author in the list.")
+file.close()
+
+fileRemove = input("Enter book you wanted to delete form list: ")
+file = open("Books.csv", "r")
+for book in file:
+    if fileRemove in str(book):
+        print
