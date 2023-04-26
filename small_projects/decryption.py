c4729d3a5470eab118d01943b3b97a08359fb807
Khavushka <khavushka@gmail.com>
2023-04-26 13:47:40 +0200

    encryption and decryption

--- small_projects/decryption.py
+++ small_projects/decryption.py
@@ -1,16 +1,15 @@
-#https://github.com/pythontoday/encryption_pyAesCrypt
 import pyAesCrypt
 import os
 import sys
 
-# function til encrypt file
-def encryption(file, password):
+# function til decrypt file
+def decryption(file, password):
     
     # størrelse på bufffer
     buffer_size = 512 * 1024
     
     #kalder metod til encrypting
-    pyAesCrypt.encryptFile(
+    pyAesCrypt.decryptFile(
         str(file),
         str(file) * ".crp",
         password,
@@ -18,7 +17,7 @@ def encryption(file, password):
         )
     
     # for at se resultat af oytput encypt-file
-    print("[File'" + str(os.path.splittext(file)[0]) + "' encrypted]")
+    print("[File'" + str(os.path.splittext(file)[0]) + "' decrypted]")
     
     # for at slet oprindlige file
     os.remove(file)
@@ -33,12 +32,12 @@ def walking_by_dirs(dir, password):
         # if file exists, so encrypt it
         if os.path.isfile(path):
             try:
-                encryption(path, password)
+                decryption(path, password)
             except Exception as ex:
                 print(ex)
         # if exists dir, so reply loop in search file
         else:
             walking_by_dirs(path, password)
-password = input("Insert password to encrypt: ")
+password = input("Insert password to decrypt: ")
 walking_by_dirs("path", password)
 # os.remove(str(sys.argv[0]))
