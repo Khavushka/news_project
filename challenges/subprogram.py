779c1ad476a50fdd9d03c7566384b3b17a60fe54
Khavushka <khavushka@gmail.com>
2023-04-27 19:55:04 +0200

    four different functions

--- challenges/subprogram.py
+++ challenges/subprogram.py
@@ -23,7 +23,7 @@ Display the following menu to the user:
 2. subtraction
 Enter 1 or 2:
 
-if they enter 1, it sholud run a subprogram that will generate two random numbers between 5 and 20, and ask the user to add them together. Work out the correct answer and return both the user's answer and the correct answer. 
+if they enter 1, it should run a subprogram that will generate two random numbers between 5 and 20, and ask the user to add them together. Work out the correct answer and return both the user's answer and the correct answer. 
 
 if they entered 2 as their selection on the menu, it should run a subprogram that will generate one number between 25 and 50 and another number between 1 and 25 and ask them to work out num1 minus num2. this way they will not have to worry about negative answers. return both the user's answer and the correct answer.
 
@@ -31,17 +31,35 @@ create another subprogram that will check if the user's answer matches the actua
 
 if they do not select a relevant option on the first menu you should display a suitable message
 '''
-print("1.addition")
-print("2.substraction")
-input_user = int(input("Enter 1 or 2: "))
+import random
+
+# print("1.addition")
+# print("2.substraction")
+# input_user = int(input("Enter 1 or 2: "))
 def addition():
-    if input_user == 1:
-        pass
+    num1 = random.randint(5,20)
+    num2 = random.randint(5,20)
+    print(num1, "+", num2, "=")
+    user_answer = int(input("Your answer: "))
+    actual_answer = num1 + num2
+    answers = (user_answer, actual_answer)
+    return answers
         
 
 def substraction():
-    if input_user == 2:
-        pass
+    num1 = random.randint(25, 50)
+    num2 = random.randint(25, 50)
+    print(num1, "-", num2, "=")
+    user_answer = int(input("Your answer: "))
+    actual_answer = num1 - num2
+    answers = (user_answer, actual_answer)
+    return answers
+
+def check_answer(user_answer, actual_answer):
+    if user_answer == actual_answer:
+        print("Correct")
+    else:
+        print("incorrect, the answer", actual_answer)
     
 def main():
     user_name = addition()
