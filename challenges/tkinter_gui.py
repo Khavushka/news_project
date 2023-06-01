34a2bb608d7dd280ee63baaf779233added2c512
Khavushka <khavushka@gmail.com>
2023-06-01 21:44:30 +0200

    GUI (graphical user interface)

---
+++ challenges/tkinter_gui.py
@@ -0,0 +1,5 @@
+'''
+A GUI (graphical user interface) makes the program easier to use. It allows you, as the programmer, to create screens, text boxes and buttons to help nvigate through the program in a more user-friendly way. Tkinter is a library of features in Python that allows you to do this.
+'''
+
+from tkinter 
