4f829bfccd4f00cef6bc025d6c7083144f54448d
Khavushka <khavushka@gmail.com>
2023-06-01 21:48:49 +0200

    Button/-Window

--- challenges/tkinter_gui.py
+++ challenges/tkinter_gui.py
@@ -2,4 +2,14 @@
 A GUI (graphical user interface) makes the program easier to use. It allows you, as the programmer, to create screens, text boxes and buttons to help nvigate through the program in a more user-friendly way. Tkinter is a library of features in Python that allows you to do this.
 '''
 
-from tkinter 
+from tkinter import *
+
+def Call():
+    msg = Label(window, text = 'You pressed the button')
+    msg.place(x = 30, y = 50)
+    button['bg'] = 'blue'
+    button['fg'] = 'white'
+    
+window = Tk()
+window.geometry(x = 30, y = 50)
+button[bg] 
