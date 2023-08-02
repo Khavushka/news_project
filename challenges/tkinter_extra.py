ccbe55e85fee21bc1892f45a4c6dc20c93131d0c
Khavushka <khavushka@gmail.com>
2023-08-02 17:12:24 +0200

    a program to enter a number

---
+++ challenges/tkinter_extra.py
@@ -0,0 +1,6 @@
+'''
+create a program that will ask the user to enter a number in a box. when they click on a button it will add that number to a total and display it in another box. this can be repeated as many times as they want and keep adding to the total. there should be another button that resets the total back to 0 and empties the original text box, ready for them to start again.
+'''
+
+from tkinter import *
+
