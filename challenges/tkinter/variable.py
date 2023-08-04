e9939dfbd05ff43ead942d98098e049e0a8a2d63
Khavushka <khavushka@gmail.com>
2023-08-04 18:28:43 +0200

    New challenge

---
+++ challenges/tkinter/variable.py
@@ -0,0 +1,4 @@
+'''
+Create a window that will ask the user to enter a number in a text box. when they click on a button it will use the code variable.isdigit() to check to see if it is a whole number. if it is a whole number, add it to a list box. add another button that will clear the list
+'''
+
