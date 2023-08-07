1521b8f6e02bfe5151aca92414b3eeeb69267019
Khavushka <khavushka@gmail.com>
2023-08-07 08:50:21 +0200

    New program

---
+++ challenges/tkinter/create_csv.py
@@ -0,0 +1,13 @@
+'''
+create a program that will allow the user to create a new .csv file. it should ask them to enter the name and age of a person and then allow them to add this to the end of the file they have just created
+'''
+
+from tkinter import *
+
+def create_new():
+    pass
+
+window = Tk()
+window.title("Program that will allow the user to create a new .csv")
+window.geometry("400x250")
+
