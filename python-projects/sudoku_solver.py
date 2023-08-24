7667a9bc532444f68860d2fa3243d21b7182a696
Khavushka <khavushka@gmail.com>
2023-08-24 21:01:35 +0200

    Sudoku solver

--- python-projects/sudoku_solver.py
+++ python-projects/sudoku_solver.py
@@ -2,3 +2,57 @@
 solving sudoku challenge. problem-solving skills. Inthis project, you will build a sudoku solver using Python and the backtracking algorithm. you will learn how to represent the puzzle, implement the solver, and visualize the solution using a graphical interface
 '''
 
+def is_valid(board, row, col, num):
+    # check if the number already exists in the row
+    for i in range(9):
+        if board[row][i] == num:
+            return False
+        
+    # check if the number already exists in the 3x3 grid
+    for i in range(9):
+        if board[i][col] == num:
+            return False
+        
+    return False
+
+def solve_sudoku(board):
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                for num in range(1, 10):
+                    if is_valid(board, row, col, num):
+                        board[row][col] = num
+                        
+                        if solve_sudoku(board):
+                            return True
+                        
+                        board[row][col] = 0
+                        
+                return False
+            
+    return True
+
+def print_board(board):
+    for row in range(9):
+        for col in range(9):
+            print(board[row][col], end = "")
+        print()
+        
+    # Example Sudoku board (0 represents empty cells)
+    board = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    ]
+    
+    if solve_sudoku(board):
+        print("Sudoku solved:")
+        print_board(board)
+    else:
+        print("No solution exists for the given Sudoku board")
