4525c48675d27cbb7f88c841db23cc3210bb8789
Khavushka <khavushka@gmail.com>
2023-08-24 21:09:05 +0200

    Finally

--- python-projects/sudoku_solver.py
+++ python-projects/sudoku_solver.py
@@ -13,7 +13,15 @@ def is_valid(board, row, col, num):
         if board[i][col] == num:
             return False
         
-    return False
+    # Check if the number already exists in the 3x3 grid
+    start_row = (row // 3) * 3
+    start_col = (col // 3) * 3
+    for i in range(3):
+        for j in range(3):
+            if board[start_row + i][start_col + j] == num:
+                return False
+
+    return True
 
 def solve_sudoku(board):
     for row in range(9):
@@ -35,24 +43,24 @@ def solve_sudoku(board):
 def print_board(board):
     for row in range(9):
         for col in range(9):
-            print(board[row][col], end = "")
+            print(board[row][col], end = " ")
         print()
         
-    # Example Sudoku board (0 represents empty cells)
-    board = [
-        [5, 3, 0, 0, 7, 0, 0, 0, 0],
-        [6, 0, 0, 1, 9, 5, 0, 0, 0],
-        [0, 9, 8, 0, 0, 0, 0, 6, 0],
-        [8, 0, 0, 0, 6, 0, 0, 0, 3],
-        [4, 0, 0, 8, 0, 3, 0, 0, 1],
-        [7, 0, 0, 0, 2, 0, 0, 0, 6],
-        [0, 6, 0, 0, 0, 0, 2, 8, 0],
-        [0, 0, 0, 4, 1, 9, 0, 0, 5],
-        [0, 0, 0, 0, 8, 0, 0, 7, 9]
-    ]
-    
-    if solve_sudoku(board):
-        print("Sudoku solved:")
-        print_board(board)
-    else:
-        print("No solution exists for the given Sudoku board")
+# Example Sudoku board (0 represents empty cells)
+board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+if solve_sudoku(board):
+    print("Sudoku solved:")
+    print_board(board)
+else:
+    print("No solution exists for the given Sudoku board")
