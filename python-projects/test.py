38f76b792be6759053c1f0486e1da3e2474b1d85
Khavushka <khavushka@gmail.com>
2023-09-07 17:16:46 +0200

    Number formatting

---
+++ python-projects/test.py
@@ -0,0 +1,13 @@
+# with  in f-strings debugging, you can also perform math operations
+
+# 1. Debug mode
+x = 10
+y = 20
+
+print(f"x ={x}, y = {y}")
+print(f"{x = }, {y =}")
+
+# math operations
+print(f"{ x * y = }")
+
+#  2. Number formatting
