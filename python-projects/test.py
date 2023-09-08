c29d59c72f32c85f71fafa35cd56c3edb1f9f6d9
Khavushka <khavushka@gmail.com>
2023-09-08 11:37:21 +0200

    number formatting

--- python-projects/test.py
+++ python-projects/test.py
@@ -1,5 +1,11 @@
 # with  in f-strings debugging, you can also perform math operations
 
+'''
+the first one is the debug feature with f-strings.
+instead of writing out "variable = ' '  ", do {variable = } instead.
+this saves a lot of time and effort, as well as makes your code look cleaner. within f-strings debugging, you can also perform math operations  in the last line.
+'''
+
 # 1. Debug mode
 x = 10
 y = 20
@@ -10,4 +16,43 @@ print(f"{x = }, {y =}")
 # math operations
 print(f"{ x * y = }")
 
-#  2. Number formatting
+#  2. Number formatting 
+'''
+there are various formatting/conversions you can perform with strings. the following are done below
+- set decimal places :.nf where n is the number of decimal places
+- hex conversion
+- binary conversion
+- octal conversion
+- scientific notation
+- pad number with leading zeros :0n where n is the total number of characters
+'''
+
+number = 420
+
+# decimal places 
+print(f"number: {number:.2f}")
+
+#  hex converion 
+print(f"hex: {number:#0x}")
+
+#  binary conversion
+print(f"binary: {number:b}")
+
+# octal conversion
+print(f"octal: {number:o}")
+
+#  scientific notation
+print(f"scientific: {number:o}")
+
+#  total number of characters
+print(f"Number: {number:09}")
+
+'''
+let's say you have a large number, as large as Apple's market cap; you can use :, where , is the separator or, if you want f string out a percentage value, you can use :.2% telling Python to set 2 decimal places and add a percentage sign to the end of the string. 
+'''
+apple_marketcap = 2.626 * 10e12
+print(f"{apple_marketcap = :,}")
+
+percentage = 10.394394
+print(f"{percentage = :.2%}")
+
