9ae3fc06fa5a32e326c15bc1b6b7e18ee8dd6439
Khavushka <khavushka@gmail.com>
2023-09-20 21:23:15 +0200

    no such file or directory

--- challenges/tkinter/more_Tkinter.py
+++ challenges/tkinter/more_Tkinter.py
@@ -7,4 +7,51 @@
 - added a drop down option entitled Select Name, which will display three names; "Bob", "Sue", "Tim" to the user;
 - added a second image in the lower half of the window which will change to show the photograph of the person selected from the drop-down list when the user clicks on the Click Me button.
 '''
+import tkinter as tk
 
+window = tk.Tk()
+window.title("My Window")
+window.geometry("400x300")
+
+# Define global variables for images
+logo = tk.PhotoImage(file="logo.gif")
+bob_image = tk.PhotoImage(file="Bob.gif")
+sue_image = tk.PhotoImage(file="Sue.gif")
+tim_image = tk.PhotoImage(file="Tim.gif")
+
+# Function to update the displayed image
+def update_image():
+    sel = selectName.get()
+    mesg = "Hello " + sel
+    mlabel["text"] = mesg
+
+    if sel == "Bob":
+        photobox["image"] = bob_image
+    elif sel == "Sue":
+        photobox["image"] = sue_image
+    elif sel == "Tim":
+        photobox["image"] = tim_image
+    else:
+        photobox["image"] = logo
+
+# Set the window icon
+window.wm_iconbitmap("MyIcon.ico")
+
+# Set the background color
+window.configure(background="light green")
+
+# Create a label for displaying text
+mlabel = tk.Label(window, text="")
+mlabel.pack()
+
+# Create an option menu
+selectName = tk.StringVar(window)
+selectName.set("Select Name")
+nameList = tk.OptionMenu(window, selectName, "Bob", "Sue", "Tim", command=update_image)
+nameList.pack()
+
+# Create a label for displaying images
+photobox = tk.Label(window, image=logo)
+photobox.pack()
+
+window.mainloop()
