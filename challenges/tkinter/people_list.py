94d5d5157a5ab6b409a03531c64405de180c6ba9
Khavushka <khavushka@gmail.com>
2023-09-27 14:55:46 +0200

    Initial commit

--- challenges/tkinter/people_list.py
+++ challenges/tkinter/people_list.py
@@ -66,5 +66,5 @@ window.mainloop()
 
 
 '''
-Using the .csv file you created for the last challenge, create a program that will allow people to add names and ages to the list and create a button that will display the contents of the .csv file by importing it to a list box. 
+Using the .csv file you created for the last challenge, create a program that will allow people to add names and ages to the list and create a button that will display the contents of the .csv file by importing it to a list box.  DONE
 '''
