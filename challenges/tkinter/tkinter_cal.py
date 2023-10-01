8b6465705bb67a81e999731ff2f8513439aaae0c
Khavushka <khavushka@gmail.com>
2023-10-01 23:00:43 +0200

    Up to date

--- challenges/tkinter/tkinter_cal.py
+++ challenges/tkinter/tkinter_cal.py
@@ -75,6 +75,7 @@ def save_list():
 
 window = Tk()
 window.title("My Tkinter calculator")
+window.configure(background = "purple")
 window.geometry("600x300")
 
 label1 = Label(text="Enter first number:")
@@ -84,23 +85,23 @@ num_box1 = Entry()
 num_box1.place(x=150, y=20, width=100, height=25)
 num_box1.focus()
 
-button1 = Button(text="+", command=plus)
+button1 = Button(text="+", command=plus, fg='white', bg='blue')
 button1.place(x=270, y=20, width=30, height=25)
 
-button2 = Button(text="-", command=substraction)
+button2 = Button(text="-", command=substraction, fg='white', bg='orange')
 button2.place(x=310, y=20, width=30, height=25)
 
-button3 = Button(text="*", command=multiple)
+button3 = Button(text="*", command=multiple, fg='white', bg='green')
 button3.place(x=350, y=20, width=30, height=25)
 
-button4 = Button(text="/", command=division)
+button4 = Button(text="/", command=division, fg='white', bg='red')
 button4.place(x=390, y=20, width=30, height=25)
 
 label2 = Label(text = "Enter second number:")
 label2.place(x=20, y=60, width=140, height=25)
 
 num_box2 = Entry()
-num_box2.place(x=150, y=60, width=100, height=25)
+num_box2.place(x=155, y=60, width=100, height=25)
 
 add_button = Button(text="Add", command=add_to_list)
 add_button.place(x=270, y=60, width=50, height=25)
@@ -109,7 +110,7 @@ num_list = Listbox()
 num_list.place(x=20, y=90, width=400, height=100)
 
 button5 = Button(text ="Clear list", command=clear_list)
-button5.place(x=270, y=250, width=100, height=25)
+button5.place(x=310, y=250, width=100, height=25)
 
 button6 = Button(text="Save list", command = save_list)
 button6.place(x=150, y=250, width=100, height=25)
