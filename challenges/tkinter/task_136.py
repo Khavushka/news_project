cc6849a7c95f60039c1db5b08276286f3e3474fb
Khavushka <khavushka@gmail.com>
2023-10-03 07:20:03 +0200

    More

---
+++ challenges/tkinter/task_136.py
@@ -0,0 +1,4 @@
+'''
+Create a program that will ask the user to enter a name and then select the gender for that person from a drop-down list. It should then add the name and the gender (separated by a comma) to alist box when the user clicks on a button
+'''
+
